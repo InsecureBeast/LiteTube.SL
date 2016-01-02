213151a0572ca10134f5e7f6c14ede200eb3a95a
Dmitriev <Dmitriev@localhost>
2016-01-02 16:42:43 +0300

    Find button on searchPage

--- LiteTube/SearchPage.xaml.cs
+++ LiteTube/SearchPage.xaml.cs
@@ -60,5 +60,11 @@ namespace LiteTube
                 SearchBox.IsReadOnly = false;
             }
         }
+
+        private void Find_Click(object sender, EventArgs e)
+        {
+            SearchBox.Focus();
+            SearchBox.SelectAll();
+        }
     }
 }
