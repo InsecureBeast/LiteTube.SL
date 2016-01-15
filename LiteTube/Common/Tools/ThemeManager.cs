2d1d49ea682bf6af603b5da8d76738237e9d7776
Dmitriev <Dmitriev@localhost>
2016-01-15 11:42:38 +0300

    ThemeManager

---
+++ LiteTube/Common/Tools/ThemeManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace LiteTube.Common.Tools
+{
+    static class ThemeManager
+    {
+        public static void GoToLightTheme()
+        {
+            var accentBrush = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+            if (accentBrush != null)
+            {
+                var accentColor = accentBrush.Color;
+                var accentLightColor = accentColor.Lerp(Colors.White, 0.25f);
+                var accentDarkColor = accentColor.Lerp(Colors.Black, 0.25f);
+
+                var accentDarkBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
+                if (accentDarkBrush != null)
+                    accentDarkBrush.Color = accentDarkColor;
+
+                var accentLightBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
+                if (accentLightBrush != null)
+                    accentLightBrush.Color = accentLightColor;
+
+                var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
+                if (backgroundBrush != null)
+                    backgroundBrush.Color = Color.FromArgb(255, 241, 240, 238); //#FFF1F0EE
+
+                var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
+                if (mainForegroundBrush != null)
+                    mainForegroundBrush.Color = Colors.Black;
+
+                var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
+                if (contentForegroundBrush != null)
+                    contentForegroundBrush.Color = Color.FromArgb(255, 93, 93, 93);
+            }
+        }
+
+        public static void GoToDarkTheme()
+        {
+            var accentBrush = Application.Current.Resources["PhoneAccentBrush"] as SolidColorBrush;
+            if (accentBrush != null)
+            {
+                var accentColor = accentBrush.Color;
+                var accentLightColor = accentColor.Lerp(Colors.White, 0.25f);
+                var accentDarkColor = accentColor.Lerp(Colors.Black, 0.25f);
+
+                var accentDarkBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
+                if (accentDarkBrush != null)
+                    accentDarkBrush.Color = accentDarkColor;
+
+                var accentLightBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
+                if (accentLightBrush != null)
+                    accentLightBrush.Color = accentLightColor;
+
+                var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
+                if (backgroundBrush != null)
+                    backgroundBrush.Color = Colors.Black;
+
+                var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
+                if (mainForegroundBrush != null)
+                    mainForegroundBrush.Color = Colors.White;
+
+                var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
+                if (contentForegroundBrush != null)
+                    contentForegroundBrush.Color = Color.FromArgb(153, 255, 255, 255); //#99FFFFFF
+            }
+        }
+    }
+}
