4223628fc85cd1b8c4e6d4110c79c4992ca42fa7
Dmitriev <Dmitriev@localhost>
2016-03-14 19:12:27 +0300

    ????. ? ????? ?????????? ????? ?? ?????!

--- LiteTube/Common/Tools/ThemeManager.cs
+++ LiteTube/Common/Tools/ThemeManager.cs
@@ -21,35 +21,35 @@ namespace LiteTube.Common.Tools
                 var accentLightBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
                 if (accentLightBrush != null)
                     accentLightBrush.Color = accentLightColor;
-
-                var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
-                if (backgroundBrush != null)
-                    backgroundBrush.Color = Color.FromArgb(255, 241, 240, 238); //#FFF1F0EE
-
-                var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
-                if (mainForegroundBrush != null)
-                    mainForegroundBrush.Color = Colors.Black;
-
-                var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
-                if (contentForegroundBrush != null)
-                    contentForegroundBrush.Color = Color.FromArgb(255, 93, 93, 93);
-
-                var secondaryBackgroundBrush = Application.Current.Resources["PhoneSecondaryBackgroundBrush"] as SolidColorBrush;
-                if (secondaryBackgroundBrush != null)
-                    secondaryBackgroundBrush.Color = Color.FromArgb(255, 230, 231, 232);
-
-                var phoneSecondaryForegroundBrush = Application.Current.Resources["PhoneSecondaryForegroundBrush"] as SolidColorBrush;
-                if (phoneSecondaryForegroundBrush != null)
-                    phoneSecondaryForegroundBrush.Color = Color.FromArgb(255, 102, 102, 102);
-
-                var disabledBackgroundBrush = Application.Current.Resources["PhoneDisabledBrush"] as SolidColorBrush;
-                if (disabledBackgroundBrush != null)
-                    disabledBackgroundBrush.Color = Color.FromArgb(255, 165, 165, 165);
-
-                var phoneBorderBrush = Application.Current.Resources["PhoneBorderBrush"] as SolidColorBrush;
-                if (phoneBorderBrush != null)
-                    phoneBorderBrush.Color = Color.FromArgb(178, 0, 0, 0); //75% of black
             }
+            
+            var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
+            if (backgroundBrush != null)
+                backgroundBrush.Color = Color.FromArgb(255, 241, 240, 238); //#FFF1F0EE
+
+            var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
+            if (mainForegroundBrush != null)
+                mainForegroundBrush.Color = Colors.Black;
+
+            var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
+            if (contentForegroundBrush != null)
+                contentForegroundBrush.Color = Color.FromArgb(255, 93, 93, 93);
+
+            var secondaryBackgroundBrush = Application.Current.Resources["PhoneSecondaryBackgroundBrush"] as SolidColorBrush;
+            if (secondaryBackgroundBrush != null)
+                secondaryBackgroundBrush.Color = Color.FromArgb(255, 230, 231, 232);
+
+            var phoneSecondaryForegroundBrush = Application.Current.Resources["PhoneSecondaryForegroundBrush"] as SolidColorBrush;
+            if (phoneSecondaryForegroundBrush != null)
+                phoneSecondaryForegroundBrush.Color = Color.FromArgb(255, 102, 102, 102);
+
+            var disabledBackgroundBrush = Application.Current.Resources["PhoneDisabledBrush"] as SolidColorBrush;
+            if (disabledBackgroundBrush != null)
+                disabledBackgroundBrush.Color = Color.FromArgb(255, 165, 165, 165);
+
+            var phoneBorderBrush = Application.Current.Resources["PhoneBorderBrush"] as SolidColorBrush;
+            if (phoneBorderBrush != null)
+                phoneBorderBrush.Color = Color.FromArgb(178, 0, 0, 0); //75% of black
         }
 
         public static void GoToDarkTheme()
@@ -68,35 +68,35 @@ namespace LiteTube.Common.Tools
                 var accentLightBrush = Application.Current.Resources["PhoneDarkAccentBrush"] as SolidColorBrush;
                 if (accentLightBrush != null)
                     accentLightBrush.Color = accentLightColor;
-
-                var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
-                if (backgroundBrush != null)
-                    backgroundBrush.Color = Colors.Black;
-
-                var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
-                if (mainForegroundBrush != null)
-                    mainForegroundBrush.Color = Colors.White;
-
-                var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
-                if (contentForegroundBrush != null)
-                    contentForegroundBrush.Color = Color.FromArgb(153, 255, 255, 255); //#99FFFFFF
-
-                var secondaryBackgroundBrush = Application.Current.Resources["PhoneSecondaryBackgroundBrush"] as SolidColorBrush;
-                if (secondaryBackgroundBrush != null)
-                    secondaryBackgroundBrush.Color = Color.FromArgb(255/2, 0, 0, 0);
-
-                var disabledBackgroundBrush = Application.Current.Resources["PhoneDisabledBrush"] as SolidColorBrush;
-                if (disabledBackgroundBrush != null)
-                    disabledBackgroundBrush.Color = Color.FromArgb(255, 165, 165, 165);
-
-                var phoneBorderBrush = Application.Current.Resources["PhoneBorderBrush"] as SolidColorBrush;
-                if (phoneBorderBrush != null)
-                    phoneBorderBrush.Color = Color.FromArgb(178, 255, 255, 255); //75% of white
-
-                var phoneSecondaryForegroundBrush = Application.Current.Resources["PhoneSecondaryForegroundBrush"] as SolidColorBrush;
-                if (phoneSecondaryForegroundBrush != null)
-                    phoneSecondaryForegroundBrush.Color = Color.FromArgb(153, 0, 0, 0);
             }
+            
+            var backgroundBrush = Application.Current.Resources["PhoneBackgroundBrush"] as SolidColorBrush;
+            if (backgroundBrush != null)
+                backgroundBrush.Color = Colors.Black;
+
+            var mainForegroundBrush = Application.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush;
+            if (mainForegroundBrush != null)
+                mainForegroundBrush.Color = Colors.White;
+
+            var contentForegroundBrush = Application.Current.Resources["PhoneSubtleBrush"] as SolidColorBrush;
+            if (contentForegroundBrush != null)
+                contentForegroundBrush.Color = Color.FromArgb(153, 255, 255, 255); //#99FFFFFF
+
+            var secondaryBackgroundBrush = Application.Current.Resources["PhoneSecondaryBackgroundBrush"] as SolidColorBrush;
+            if (secondaryBackgroundBrush != null)
+                secondaryBackgroundBrush.Color = Color.FromArgb(255/2, 0, 0, 0);
+
+            var disabledBackgroundBrush = Application.Current.Resources["PhoneDisabledBrush"] as SolidColorBrush;
+            if (disabledBackgroundBrush != null)
+                disabledBackgroundBrush.Color = Color.FromArgb(255, 165, 165, 165);
+
+            var phoneBorderBrush = Application.Current.Resources["PhoneBorderBrush"] as SolidColorBrush;
+            if (phoneBorderBrush != null)
+                phoneBorderBrush.Color = Color.FromArgb(178, 255, 255, 255); //75% of white
+
+            var phoneSecondaryForegroundBrush = Application.Current.Resources["PhoneSecondaryForegroundBrush"] as SolidColorBrush;
+            if (phoneSecondaryForegroundBrush != null)
+                phoneSecondaryForegroundBrush.Color = Color.FromArgb(153, 0, 0, 0);
         }
     }
 }
