7e74b794422e4a3b04ff542738832e7d5bde455a
Dmitriev <Dmitriev@localhost>
2016-04-05 23:23:26 +0300

    LiteTube for Windows 10 begin

--- LiteTube/Common/SuspensionManager.cs
+++ LiteTube/Common/SuspensionManager.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using Windows.ApplicationModel;
 using Windows.Storage;
 using Windows.Storage.Streams;
 
