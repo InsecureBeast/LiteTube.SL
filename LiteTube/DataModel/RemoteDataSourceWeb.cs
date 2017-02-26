865d43c714bb38afc769bd9f2797c95b0303b88b
Dmitriev <Dmitriev@localhost>
2017-02-26 15:36:51 +0300

    GetMostPopular ???????? ?????? ? ??????? ?????

---
+++ LiteTube/DataModel/RemoteDataSourceWeb.cs
@@ -0,0 +1,23 @@
+﻿using Google.Apis.YouTube.v3;
+using LiteTube.Common;
+using LiteTube.DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteTube.DataModel
+{
+    partial class RemoteDataSource
+    {
+        private async Task<IVideoList> GetMostPopularWeb(string culture, int maxResult, string pageToken)
+        {
+            var res = await _youTubeWeb.GetMostPopular(culture, pageToken);
+            if (res == null)
+                return MVideoList.Empty;
+
+            return await GetVideoList(res);
+        }
+    }
+}
